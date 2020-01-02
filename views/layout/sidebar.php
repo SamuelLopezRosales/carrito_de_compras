48f337b9c0744ef27d917b5807752598036867e3
SamuelLopezRosales <35552072+SamuelLopezRosales@users.noreply.github.com>
2020-01-02 00:50:12 -0600

    Productos de la tienda
    En este commit se realizo un crud de los productos actuales en la tienda

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -18,7 +18,7 @@
                     
                     <?php if(isset($_SESSION['admin'])): ?>
                     <li><a href="<?=base_url?>Categoria/index">Gestionar Categorias</a></li>
-                    <li><a href="#">Gestionar Productos</a></li>
+                    <li><a href="<?=base_url?>Producto/gestion">Gestionar Productos</a></li>
                     <li><a href="#">Gestionar Pedidos</a></li>
                     <?php endif; ?>
 
